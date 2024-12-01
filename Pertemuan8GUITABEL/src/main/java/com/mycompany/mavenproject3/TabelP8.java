445f972e1b28094a552a85bca870f1708a9afc11
ZCKHGL <164823089+ZCKHGL@users.noreply.github.com>
2024-12-02 04:03:49 +0700

    Add files via upload

---
+++ Pertemuan8GUITABEL/src/main/java/com/mycompany/mavenproject3/TabelP8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author excee
+ */
+public class TabelP8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
